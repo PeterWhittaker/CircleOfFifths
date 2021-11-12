64d485811aef7310e22f93351be62780a8a2fb44
Peter Whittaker <PeterWhittaker@SphyrnaSecurity.com>
2021-11-12 11:41:27 -0500

    Add doc re major scale modes and intervals

--- fifths.js
+++ fifths.js
@@ -81,3 +81,24 @@ class Scale {
     constructor(tonic = 'C') {
     }
 }
+/*
+Ionian  	I	W–W–H–W–W–W–H	C–D–E–F–G–A–B–C
+Dorian  	ii	W–H–W–W–W–H–W	D–E–F–G–A–B–C–D
+Phrygian	iii	H–W–W–W–H–W–W	E–F–G–A–B–C–D–E
+Lydian  	IV	W–W–W–H–W–W–H	F–G–A–B–C–D–E–F
+Mixolydian	V	W–W–H–W–W–H–W	G–A–B–C–D–E–F–G
+Aeolian 	vi	W–H–W–W–H–W–W	A–B–C–D–E–F–G–A
+Locrian 	viiø	H–W–W–H–W–W–W	B–C–D–E–F–G–A–B
+
+Intervals with respect to the tonic
+
+MODE        White note  unison  second  third   fourth          fifth           sixth   seventh octave
+Lydian  	F	perfect	major	major	augmented	perfect	        major	major	perfect
+Ionian  	C	perfect major   major   perfect         perfect         major   major   perfect
+Mixolydian	G	perfect major   major   perfect         perfect         major   minor   perfect
+Dorian  	D	perfect major   minor   perfect         perfect         major   minor   perfect
+Aeolian 	A	perfect major   minor   perfect         perfect         minor   minor   perfect
+Phrygian	E	perfect minor   minor   perfect         perfect         minor   minor   perfect
+Locrian 	B	perfect minor   minor   perfect         diminished      minor   minor   perfect
+*/
+
